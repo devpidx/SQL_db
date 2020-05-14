9aa9b3ed069539da0a8f89288cc39688677971ba
katigrem <kgremillion@pidx.org>
2020-05-14 15:22:36 -0500

    Pushing DB file & test api py file

---
+++ product_codes_queries.sql
@@ -0,0 +1,11 @@
+CREATE TABLE product_codes (
+	code VARCHAR(6) PRIMARY KEY,
+	product_definition VARCHAR(50) NOT NULL,
+	description VARCHAR (100),
+	cetane_octane VARCHAR (10),
+	oxygenated_rbob_type VARCHAR(10),
+	oxygenate_percent VARCHAR (10),
+	comments_ VARCHAR (100),
+	requester VARCHAR (50),
+	date_code_assigned VARCHAR (11)
+);
