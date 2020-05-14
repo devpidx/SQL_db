8f8c5b1def4c27a12d651cd3602034fdff1c7ea5
katigrem <kgremillion@pidx.org>
2020-05-14 16:19:29 -0500

    Test & Product apis

--- projects/api/api.py
+++ projects/api/api.py
@@ -1,27 +1,15 @@
 import flask
 from flask import request, jsonify
+import sqlite3
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-# Create some test data for our catalog in the form of a list of dictionaries.
-books = [
-    {'id': 0,
-     'title': 'A Fire Upon the Deep',
-     'author': 'Vernor Vinge',
-     'first_sentence': 'The coldsleep itself was dreamless.',
-     'year_published': '1992'},
-    {'id': 1,
-     'title': 'The Ones Who Walk Away From Omelas',
-     'author': 'Ursula K. Le Guin',
-     'first_sentence': 'With a clamor of bells that set the swallows soaring, the Festival of Summer came to the city Omelas, bright-towered by the sea.',
-     'published': '1973'},
-    {'id': 2,
-     'title': 'Dhalgren',
-     'author': 'Samuel R. Delany',
-     'first_sentence': 'to wound the autumnal city.',
-     'published': '1975'}
-]
+def dict_factory(cursor, row):
+    d = {}
+    for idx, col in enumerate(cursor.description):
+        d[col[0]] = row[idx]
+    return d
 
 @app.route('/', methods=['GET'])
 def home():
@@ -29,31 +17,50 @@ def home():
         <p>This site is a prototype API for distant reading of science fiction novels.</p>"
 
 # A route to return all of the available entries in our catalog.
-@app.route('/api/v1/resources/books/all', methods=['GET', 'POST', 'PUT'])
+@app.route('/api/v1/resources/books/all', methods=['GET', 'POST', 'PUSH'])
 def api_all():
-    return jsonify(books)
+    conn = sqlite3.connect('product_codes.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+    all_product_codes = cur.execute('SELECT * FROM product_codes;').fetchall()
+
+    return jsonify(all_product_codes)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return "<h1>404</h1><p>The resource could not be found.</p>", 404
 
 @app.route('/api/v1/resources/books', methods=['GET'])
-def api_id():
-    # Check if an ID was provided as part of the URL.
-    # If ID is provided, assign it to a variable.
-    # If no ID is provided, display an error in the browser.
-    if 'id' in request.args:
-        id = int(request.args['id'])
-    else:
-        return "Error: No id field provided. Please specify an id."
+def api_filter():
+    query_parameters = request.args
 
-    # Create an empty list for our results
-    results = []
+    code = query_parameters.get('code')
+    product_definition = query_parameters.get('product_definition')
+    description = query_parameters.get('description')
 
-    # Loop through the data and match results that fit the requested ID.
-    # IDs are unique, but other fields might return many results
-    for book in books:
-        if book['id'] == id:
-            results.append(book)
+    query = "SELECT * FROM books WHERE"
+    to_filter = []
 
-    # Use the jsonify function from Flask to convert our list of
-    # Python dictionaries to the JSON format.
+    if code:
+        query += ' code=? AND'
+        to_filter.append(code)
+    if product_definition:
+        query += ' product_definition=? AND'
+        to_filter.append(product_definition)
+    if description:
+        query += ' description=? AND'
+        to_filter.append(description)
+    if not (code or product_definition or description):
+        return page_not_found(404)
+
+    query = query[:-4] + ';'
+
+    conn = sqlite3.connect('product_codes.db')
+    conn.row_factory = dict_factory
+    cur = conn.cursor()
+
+    results = cur.execute(query, to_filter).fetchall()
+
     return jsonify(results)
 
 app.run()
