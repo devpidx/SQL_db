5d3ed4b3aff33483cbe5a7a8c44f8e6804f59bee
maddieIZ <mansoure.izadii@gmail.com>
2020-05-29 09:49:52 -0500

    add Java script code

---
+++ Java Script code/static/js/data.js
@@ -0,0 +1,24 @@
+var data = [{
+  code: "B78",
+  product_definition: "FUEL ETHANOL",
+  description: "ALTERNATIVE FUEL - Ed85",
+  cetane_octane: "",
+  oxygenated_rbob_type: "A",
+  oxygenate_percent: "85",
+  comments: "Ed85 85% ETHANOL/15% CARBOB" ,
+  requester : "Steve Wood",
+  date_code_assigned : "10/24/2019"
+  },
+  {
+    code: "C00",
+    product_definition: "MIDGRADE",
+    description: "CG",
+    cetane_octane: "91.5",
+    oxygenated_rbob_type: "A",
+    oxygenate_percent: "10",
+    comments: "",
+    requester :"SARA GLEASON" ,
+    date_code_assigned : "11/5/1997"
+
+  }
+];
